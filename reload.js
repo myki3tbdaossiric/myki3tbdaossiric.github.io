f2fb35fae0252bb69e613126fa629c00cbf33c0f
Stefan Mar Antonsson <stefanmarantonsson@gmail.com>
2020-09-24 14:31:23 +0000

    new look

---
+++ reload.js
@@ -0,0 +1,27 @@
+function Reload1() {
+    var iframe = document.getElementById("sb");
+    iframe.src = iframe.src;
+}
+function Reload2() {
+    var iframe = document.getElementById("kg");
+    iframe.src = iframe.src;
+}
+function Reload3() {
+    var iframe = document.getElementById("mt");
+    iframe.src = iframe.src;
+}
+function Reload4() {
+    //var url = document.getElementById("ffh").src;
+    
+    //setTimeout(SetSource("blank.html"), 2000);
+    //setTimeout(SetSource(url), 100);
+    
+    //var url = iframe.src;
+    //iframe.src = "blank.html";
+    //iframe.src = url;
+    
+    //parent.frames[ffh].window.location.reload();
+    //document.getElementById(ffh).contentDocument.location.reload(true);
+    //var asd = parent.frames.length;
+    //window.alert(asd);
+}
